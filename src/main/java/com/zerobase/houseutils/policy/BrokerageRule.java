6f21210c943d1f9dc3d614d629b99181368828c8
9898s <sueh9898@icloud.com>
2023-09-02 14:39:42 +0900

    02. 함수

---
+++ src/main/java/com/zerobase/houseutils/policy/BrokerageRule.java
@@ -0,0 +1,26 @@
+package com.zerobase.houseutils.policy;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+/**
+ * 가격이 특정 범위일 때 상환 효율과 상환 금액을 가지는 클래스
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class BrokerageRule {
+    private Double brokeragePercent;
+    private Long limitAmount;
+
+    public Long calcMaxBrokerage(Long price) {
+        if (limitAmount == null) {
+            return multiplyPercent(price);
+        }
+        return Math.min(multiplyPercent(price), limitAmount);
+    }
+
+    private Long multiplyPercent(Long price) {
+        return Double.valueOf(Math.floor(brokeragePercent / 100 * price)).longValue();
+    }
+}
